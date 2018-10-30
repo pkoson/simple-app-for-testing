3d38e7c53f33e923e56fb5ebcb10916ec7fffb32
piotrkoson <piotr@koson.pl>
2018-10-30 08:25:32 +0100

    Lesson #1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    show: false
+  };
+  handleClick = () => {
+    this.setState(prevState => ({ show: !prevState.show }));
+    this.handleRequest('firstRequest');
+  };
+  handleRequest = type => {
+    console.log('type', type);
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button className="red-button" onClick={this.handleClick}>
+          Click me
+        </button>
+        {this.state.show && <div className="section-two">SECTION TWO</div>}
       </div>
     );
   }
